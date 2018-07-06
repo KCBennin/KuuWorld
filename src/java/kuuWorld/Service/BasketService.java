aa8ec38eb7365b603577515820a685dbd91ef71d
Kuuku Benin <Bennin05@yahoo.co.uk>
2018-07-06 17:27:44 +0100

    implemented basic basket service

---
+++ src/java/kuuWorld/Service/BasketService.java
@@ -0,0 +1,20 @@
+/**
+ * Basket Service
+ *
+ * @author kuuku
+ */
+
+ package java.kuuWorld.Service;
+
+import java.kuuWorld.Entity.AccountEntity;
+
+public class BasketService {
+
+    public AccountEntity GetAccount(int Id) {
+        return new AccountEntity();
+    }
+
+    public void RemoveAccount(int Id) {
+        
+    }
+ }
