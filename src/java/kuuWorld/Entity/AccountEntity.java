74c577f25580603dea8cae1d8110a65df4bf71d5
Kuuku Benin <Bennin05@yahoo.co.uk>
2018-07-06 17:26:50 +0100

    added account entity

---
+++ src/java/kuuWorld/Entity/AccountEntity.java
@@ -0,0 +1,19 @@
+package java.kuuWorld.Entity;
+
+/**
+ * REST Web Service
+ *
+ * @author kuuku
+ */
+
+ public class AccountEntity {
+
+    public int Id;
+
+    public String FirstName;
+
+    public String SecondName;
+
+    public String Address;
+
+ }
